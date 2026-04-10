ec5fcfb7e3d6f9f48d02338b985fa0ae79bb75af
Santosh <schintawar80@gmail.com>
2026-04-10 22:36:00 +0530

    day 5

---
+++ conditional statement/one.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class one {
+    public static void main (String[]args ) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+
+        switch (number) {
+
+            case 1: System.out.println("samosa");
+            break;
+            case 2: System.out.println("pizza");
+            break;
+            case 3: System.out.println("burger");
+            break;
+            default : System.out.println(" not ");
+            break;
+
+        }
+
+        
+
+        
+    }
+    
+}
