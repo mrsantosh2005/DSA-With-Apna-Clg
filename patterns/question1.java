715a050392d3dd3cccbd539d7a04579b02471580
Santosh <schintawar80@gmail.com>
2026-04-21 11:10:40 +0530

    day7

---
+++ patterns/question1.java
@@ -0,0 +1,13 @@
+
+public class question1 {
+    public static void main(String[] args) {
+        for (int line = 10; line >= 1; line--) {
+            for (int star = line; star >= 1; star--) {
+                System.out.print(" * ");
+            }
+            System.out.println();
+
+        }
+    }
+
+}
---
+++ patterns/question2.java
@@ -0,0 +1,14 @@
+
+
+public class question2 {
+    public static void main(String[] args) {
+        int n = 10;
+        for (int i=1; i<=n; i++){
+            for (int s=1; s<=i; s++){
+            System.out.print(s);
+            }
+        
+        System.out.println();}
+    }
+    
+}
