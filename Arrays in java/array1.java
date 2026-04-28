28e638d6c955b4096a5bf66c518ca0520a48b9f5
Santosh <schintawar80@gmail.com>
2026-04-28 11:10:34 +0530

    Day 9

---
+++ Arrays in java/array1.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class array1 {
+    public static void main(String[] args) {
+        int marks[]= new int [100];
+        Scanner sc = new Scanner(System.in);
+        System.out.println(" enter your physics marks");
+        marks[0] = sc.nextInt(); // physics marks
+        System.out.println(" enter your chemistry marks");
+        marks[1] = sc.nextInt(); // chemistry mraks
+        System.out.println("enter your math marks ");
+        marks[2] = sc.nextInt(); // math marks 
+
+        System.out.println("physics marks = " + marks[0]);
+        System.out.println("chemistry marks = " + marks[1]);
+        System.out.println("math marks = " + marks[2]);
+    }
+    
+}
