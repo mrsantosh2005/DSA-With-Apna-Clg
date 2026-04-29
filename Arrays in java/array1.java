ebd58659729ab1b52a7e84d7bcedba1fdc765213
Santosh <schintawar80@gmail.com>
2026-04-29 23:37:51 +0530

    day9

--- Arrays in java/array1.java
+++ Arrays in java/array1.java
@@ -11,9 +11,15 @@ public class array1 {
         System.out.println("enter your math marks ");
         marks[2] = sc.nextInt(); // math marks 
 
+       
+
         System.out.println("physics marks = " + marks[0]);
         System.out.println("chemistry marks = " + marks[1]);
         System.out.println("math marks = " + marks[2]);
+        System.out.println("your total marks ");
+        
+         int percentage  = (marks[0] + marks[1] + marks[2]) / 3;
+        System.out.println( "percentage = " + percentage + "%");
     }
     
 }
